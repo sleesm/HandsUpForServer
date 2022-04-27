4f482ec5819edc56b14732b019e722aff2b9d461
sleesm <sleepingsm813@gmail.com>
2022-04-27 12:53:38 +0900

    HUCO-75 | [fix] fix the data access error of the similarity api result

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,36 +1,38 @@
 require('dotenv').config();
-var request = require('request');
+const axios = require('axios');
 
 async function getGameResult(req, res){
     var post = req.body;
-
-    var requestJson = {
-        'access_key': process.env.ACCESS_KEY,
-        'argument': {
-            'first_word': post.answer,
-            'second_word': post.result,
-        }
-    };
-
-    var options = {
+    let result = await axios({
+        method: "post",
         url: process.env.OPEN_API_URL,
-        body: JSON.stringify(requestJson),
-        headers: {'Content-Type':'application/json; charset=UTF-8'}
-    };
-    
-    request.post(options, function (error, response, body) {
-        console.log('responseCode = ' + response.statusCode);
-        console.log('responseBody = ' + body);
-        console.log(body.result);
-        if(body.result = 0){
-            if(body.return_object["WWN WordRelInfo"].Similarity[0] > 0.8)
+        data: {
+            'access_key': process.env.ACCESS_KEY,
+            'argument': {
+                'first_word': post.answer,
+                'second_word': post.result,
+            }
+        },
+        headers: {
+            "Content-Type": "application/json",
+            "charset": "UTF-8"
+        }
+
+    }).then((response) => {
+        simarilty = response.data.return_object["WWN WordRelInfo"].WordRelInfo.Similarity[0].SimScore;
+        if(!response.data.result){
+            if(simarilty > 0.8)
                 res.json({"result": "success", "correct" : true});
             else
             res.json({"result": "success", "correct" : false});
         }else{
             res.json({"result": "fail"});
         }
+    }).catch((error) => {
+        console.log(error);
     });
+
+    return result;
 }
 
 module.exports = {getGameResult}
