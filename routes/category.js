195790cd8412506cfd7032a715f577121e0504ed
sleesm <sleepingsm813@gmail.com>
2022-05-11 16:24:24 +0900

    HUCO-99 | [fix] change get to post

--- routes/category.js
+++ routes/category.js
@@ -5,8 +5,8 @@ var categoryController = require('../controllers/categoryController');
 //get built-in categories
 router.get('/',categoryController.getBuiltInCategory);
 
-//get built-in categories
-router.get('/custom',categoryController.getCustomCategory);
+//get custom categories
+router.post('/custom',categoryController.getCustomCategory);
 
 //create custom category
 router.post('/create', categoryController.addCustomCategory);
