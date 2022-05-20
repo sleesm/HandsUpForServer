5a2c6ba2ca2338234ef2047e15a0a04eeb1b18ac
sleesm <sleepingsm813@gmail.com>
2022-05-20 16:19:36 +0900

    HUCO-132 | [refact & feat] refactoring getting custom category function & create get public access category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -11,26 +11,26 @@ async function getBuiltInCategory(req, res) {
     }
 }
 
+//get all custom category using user_id
 async function getCustomCategory(req,res){
     var post = req.body;
-    //get all custom category using user_id
     var customResult = await categoryModel.getCustomCategoryInfo(post.user_id);
     if(!customResult)
         res.json({"result": "fail"});
-    else{
-        customResult = JSON.parse(customResult);
-        for (var cate of customResult) {
-            //get specific category info using category_id
-            var result = await categoryModel.getSpecificCateogy(cate.category_id);
-            if(!result)
-                res.json({"result": "fail"});
-            else{
-                result = JSON.parse(result);
-                cate.category_name = result.category_name;
-                cate.category_is_built_in = result.category_is_built_in;
-            }
-        }
-        var categories = customResult;
+    else {
+        var categories = JSON.parse(customResult);
+        res.json({"result": "success", "categories": categories});
+    }
+}
+
+//get public access category
+async function getPublicCategory(req,res){
+    var post = req.body;
+    var result = await categoryModel.getAllPulicCategory(post.user_id);
+    if(!result)
+        res.json({"result": "fail"});
+    else {
+        var categories = JSON.parse(result);
         res.json({"result": "success", "categories": categories});
     }
 }
@@ -60,5 +60,6 @@ async function addCustomCategory(req, res) {
 module.exports = {
     getBuiltInCategory,
     getCustomCategory,
+    getPublicCategory,
     addCustomCategory
 }
